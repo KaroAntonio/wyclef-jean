d38dc0c417b9617d8b3a71530e96e6abcad9bde6
Jemar Jones <jemarkjones@gmail.com>
2017-01-21 15:16:35 -0500

    Implemented firebase login

---
+++ app/public/javascripts/config.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+if (firebase.apps.length === 0) {
+    var config = {
+      apiKey: "AIzaSyDU62oOKXsMmT9sRWyW61V8jDPQCIjDcdo",
+      authDomain: "wyclef-jean-2655d.firebaseapp.com",
+      databaseURL: "https://wyclef-jean-2655d.firebaseio.com",
+      storageBucket: "wyclef-jean-2655d.appspot.com",
+      messagingSenderId: "528428252791"
+    };
+    firebase.initializeApp(config);
+}
