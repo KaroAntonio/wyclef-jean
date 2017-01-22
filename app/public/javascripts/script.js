8bf774e513234129438e7e5565629aa552ee0ea3
Jemar Jones <jemarkjones@gmail.com>
2017-01-21 22:08:52 -0500

    Made subscribe thing work

--- app/public/javascripts/script.js
+++ app/public/javascripts/script.js
@@ -234,7 +234,7 @@ function update_geolocation(state, center_on_success) {
 				center_map(state)
 			
 			subscribeToStrokes(state, function(stroke) {
-				console.log(stroke);
+				// console.log(stroke);
 			});
 		}
 
@@ -301,12 +301,13 @@ function finish_stroke(state) {
 
 	// Build stroke pkg
 	var stroke_data = {
-		'path_coords':curr_stroke.path,
-		'stroke_type':curr_stroke.type,
-		'tags':curr_stroke.tags,
-		'timestamp':Date.now(),
-		'stroke_color':curr_stroke.color,
-		'stroke_weight':curr_stroke.weight
+		'path_coords': curr_stroke.path,
+		'stroke_type': curr_stroke.type,
+		'tags': curr_stroke.tags,
+		'user_id': curr_stroke.user_id,
+		'timestamp': Date.now(),
+		'stroke_color': curr_stroke.color,
+		'stroke_weight': curr_stroke.weight
 	}
 	update_strokes(state)
 	saveStroke(stroke_data);		
